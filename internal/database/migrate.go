d7db28914aa3b0c8fbc3b909a6776202ad05e4da
KaZ600 <trongnghia.ngo@hotmail.com>
2023-12-10 01:27:22 +0700

    fix path with protocol 'file//' -> 'file://'

--- internal/database/migrate.go
+++ internal/database/migrate.go
@@ -3,9 +3,11 @@ package database
 import (
 	"database/sql"
 	"fmt"
-
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
+	_ "github.com/mattes/migrate/source/file"
 	"github.com/ntnghiatn/financial-app-backend/internal/config"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
@@ -17,19 +19,21 @@ func migrateDb(db *sql.DB) error {
 		return errors.Wrap(err, "connecting to database")
 	}
 
-	migrationSource := fmt.Sprintf("file//%sinternal/database/migrations", *config.Datadirectory)
-	migrator, err := migrate.NewWithDatabaseInstance(migrationSource, "postgres", driver)
+	migrationSource := fmt.Sprintf("file://%sinternal/database/migrations/", *config.DataDirectory)
+	//logrus.Info(migrationSource)
+	migrator, err := migrate.NewWithDatabaseInstance(migrationSource, "ciquandb", driver)
+	logrus.Info(migrator)
 	if err != nil {
 		return errors.Wrap(err, "creating migrator")
 	}
 
-	if err := migrator.Up(); err != nil && err != migrate.ErrNoChange {
+	if err := migrator.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
 		return errors.Wrap(err, "executing migration")
 	}
 
 	version, dirty, err := migrator.Version()
 	if err != nil {
-		return errors.Wrap(err, "getting migretion version")
+		return errors.Wrap(err, "getting migration version")
 	}
 
 	logrus.WithFields(logrus.Fields{"version": version, "dirty": dirty}).Debug("Database migrated!!")
